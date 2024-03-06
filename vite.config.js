24f32a3ab3c32dd712ddd1a9817f51a93274f050
jin <a11718trannhuy@gmail.com>
2024-03-06 08:42:48 +0700

    config import alias

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@icons": path.resolve(__dirname, "./src/assets/icons"),
+      "@images": path.resolve(__dirname, "./src/assets/images"),
+      "@core": path.resolve(__dirname, "./src/core"),
+    }
+  }
 })
